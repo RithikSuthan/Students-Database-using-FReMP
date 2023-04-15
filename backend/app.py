5d2a4b2050b83c5dfe9fbc1744c8b195b3e11024
Rithik Suthan S <72434153+RithikSuthan@users.noreply.github.com>
2023-04-15 09:31:13 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 CORS(app)
 
 
-app.config['MONGO_URI'] = 'mongodb+srv://Rithik_Suthan_S:8098329762@cluster0.nwyrzl2.mongodb.net/Student?retryWrites=true&w=majority'
+app.config['MONGO_URI'] = <Your Connection URL>
 mongo = PyMongo(app)
 
 
